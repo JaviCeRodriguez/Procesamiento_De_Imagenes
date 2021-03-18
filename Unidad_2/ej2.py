eed783cfcdd05a136d3a42cb877dcc395ff0a16d
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-03-18 08:52:46 -0300

    Elimino script Jupyter Notebook

---
+++ Unidad_2/ej2.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from skimage import data, io
+from skimage.transform import resize
+
+# image = data.astronaut()
+image = io.imread('../images/loros.jpg')
+image_sub = resize(image, (image.shape[0]//6, image.shape[1]//6))
+image_sobre = resize(image, (image.shape[0]*4, image.shape[1]*4))
+
+plt.subplot(2, 2, 3)
+plt.imshow(image_sub)
+plt.title('Imagen submuestreada')
+
+plt.subplot(2, 2, (1, 2))
+plt.imshow(image)
+plt.title('Imagen 0riginal')
+
+plt.subplot(2, 2, 4)
+plt.imshow(image_sobre)
+plt.title('Imagen sobremuestreada')
+
+plt.show()
